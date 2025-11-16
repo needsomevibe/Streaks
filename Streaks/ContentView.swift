443619e02a4dd2a704f1367a13d8d8a7267b382e
Marsel Khasanov <m.khasanov@uzum.com>
2025-11-16 14:53:18 +0000

    Initial Commit

---
+++ Streaks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Streaks
+//
+//  Created by needsomevibe on 16/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Streaks/StreaksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreaksApp.swift
+//  Streaks
+//
+//  Created by needsomevibe on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct StreaksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
